f9cdba428b02c703d8ef167c6643ee50ea8c6386
Hoppouu <drone1575@gmail.com>
2025-05-24 08:38:44 +0900

    2025-05-24

---
+++ acmicpc.net/15666.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> used;
+int solve()
+{
+
+}
+int main()
+{
+	cin >> n >> m;
+	used.assign(n, 0);
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		v.push_back(x);
+	}
+}
