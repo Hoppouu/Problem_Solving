e9f6a00388b92bc56d3fecbc2def70bc65a23111
Hoppouu <drone1575@gmail.com>
2025-05-24 09:45:29 +0000

    2025-05-24

--- acmicpc.net/15666.cpp
+++ acmicpc.net/15666.cpp
@@ -4,20 +4,44 @@
 using namespace std;
 
 int n, m;
-vector<int> v;
-vector<int> used;
-int solve()
+vector<int> v, ans;
+void print()
 {
-
+	for(int i = 0; i < m; i++)
+	{
+		cout<< ans[i] << " ";
+	}
+	cout << "\n";
+}
+void solve(int k, int x)
+{
+	if(k == m)
+	{
+		print();
+		return;
+	}
+	
+	int t = 0;
+	for(int i = x; i < v.size(); i++)
+	{
+		if(v[i] > t)
+		{
+			t = v[i];
+			ans[k] = v[i];
+			solve(k + 1, i);
+		}
+	}
 }
 int main()
 {
 	cin >> n >> m;
-	used.assign(n, 0);
+	ans.assign(n, 0);
 	for (int i = 0; i < n; i++)
 	{
 		int x;
 		cin >> x;
 		v.push_back(x);
 	}
+	sort(v.begin(), v.end());
+	solve(0, 0);
 }
