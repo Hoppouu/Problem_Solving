aee7ad869323e785153c9ac6f4aac88c74372a89
Hoppouu <drone1575@gmail.com>
2025-05-25 02:53:59 +0000

    2025-05-25

---
+++ acmicpc.net/1991.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+/*
+7
+A B C
+B D .
+C E F
+E . .
+F . G
+D . .
+G . .
+*/
+
+vector<vector<int>> v;
+void pre(int x)
+{
+    if(x == -1)
+    {
+        return;
+    }
+
+    cout << char(x + 'A');
+    pre(v[x][0]);
+    pre(v[x][1]);
+}
+void in(int x)
+{
+    if(x == -1)
+    {
+        return;
+    }
+
+    in(v[x][0]);
+    cout << char(x + 'A');
+    in(v[x][1]);
+}
+void post(int x)
+{
+    if(x == -1)
+    {
+        return;
+    }
+    post(v[x][0]);
+    post(v[x][1]);
+    cout << char(x + 'A');
+}
+
+void solve()
+{
+    pre(0);
+    puts("");
+    in(0);
+    puts("");
+    post(0);
+}
+int main()
+{
+    int n;
+    cin >> n;
+    v.assign(n + 1, vector<int>(2, -1));
+    for(int i = 0; i < n; i++)
+    {
+        string s1, s2, s3;
+        cin >> s1 >> s2 >> s3;
+
+        if(s2!= ".")
+        {
+            v[s1[0] - 'A'][0] = s2[0] - 'A';
+        }
+        if(s3 != ".")
+        {
+            v[s1[0] - 'A'][1] = s3[0] - 'A';
+        }
+    }
+    solve();
+}
