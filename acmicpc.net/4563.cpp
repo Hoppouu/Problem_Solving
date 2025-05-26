b3102400a9fa323dfd70da5f054116f4e45032b5
Hoppouu <drone1575@gmail.com>
2025-05-26 07:46:53 +0000

    solving

--- acmicpc.net/4563.cpp
+++ acmicpc.net/4563.cpp
@@ -9,13 +9,23 @@ int solve()
     // b = A / (2 * t) - t / 2
     // t는 2의 배수 이어야함.
     int A = a * a;
-    
-    for(int t = 2; t < a; t++)
+    int cnt = 0;
+    //c > a + b 이어야한다.
+    for(int t = 2; t < a * a; t++)
     {
+        if(A % (2 * t) != 0)
+        {
+            continue;
+        }
         int c = A / (2 * t) + t / 2;
         int b = A / (2 * t) - t / 2;
-        
+        if(c > b + c)
+        {
+            cout << a << " " << b << " " << c << "\n";
+            cnt++;
+        }
     }
+    return cnt;
 }
 int main()
 {
@@ -23,6 +33,6 @@ int main()
     while(T--)
     {
         cin >> a;
-
+        cout << solve() << "\n";
     }
 }
