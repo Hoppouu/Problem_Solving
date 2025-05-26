28bb39012e47eba49b7e0961dd13235d8d62272b
Hoppouu <drone1575@gmail.com>
2025-05-26 07:01:45 +0000

    2025-05-26

--- acmicpc.net/15686.cpp
+++ acmicpc.net/15686.cpp
@@ -3,12 +3,11 @@
 using namespace std;
 
 int n, m, ans = 1 << 30;
-vector<pair<int, int>> house, store;
-vector<int> selected;
-
-int dist(int x1, int y1, int x2, int y2)
+vector<pair<int, int>> house, store, selected;
+int c= 0;
+int dist(int r1, int c1, int r2, int c2)
 {
-	return abs(x1 - x2) + abs(y2 - y1);
+	return abs(r1 - r2) + abs(c1 - c2);
 }
 
 int solve1()
@@ -17,36 +16,29 @@ int solve1()
 	for (int i = 0; i < house.size(); i++)
 	{
 		int minx = 1 << 30;
-		int x1 = house[i].first, y1 = house[i].second;
-		for(int j = 0; j < store.size(); j++)
+		int r1 = house[i].first, c1 = house[i].second;
+		for(int j = 0; j < selected.size(); j++)
 		{
-			if (selected[j])
-			{
-				int x2 = store[j].first, y2 = store[j].second;
-				minx = min(dist(x1, y1, x2, y2), minx);
-			}
+			int r2 = selected[j].first, c2 = selected[j].second;
+			minx = min(dist(r1, c1, r2, c2), minx);
 		}
 		sum += minx;
 	}
-
 	return sum;
 }
 
-void solve(int k)
+void solve(int k, int a)
 {
 	if (k == m)
 	{
 		ans = min(solve1(), ans);
 		return;
 	}
-	for (int i = 0; i < store.size(); i++)
+	for (int i = a; i < store.size(); i++)
 	{
-		if (!selected[i])
-		{
-			selected[i] = 1;
-			solve(k + 1);
-			selected[i] = 0;
-		}
+		int r = store[i].first, c = store[i].second;
+		selected[k]= {r, c};
+		solve(k + 1, i + 1);
 	}
 }
 
@@ -70,8 +62,7 @@ int main()
 			}
 		}
 	}
-	selected.assign(store.size(), 0);
-	solve(0);
-
+	selected.assign(m, pair<int, int>());
+	solve(0, 0);
 	cout << ans << "\n";
 }
