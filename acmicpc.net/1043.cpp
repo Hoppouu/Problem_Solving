45a25c5f1a770b39e0908b28b780e7f6cfffa75a
Hoppouu <drone1575@gmail.com>
2025-05-28 05:54:54 +0000

    2025-05-28

--- acmicpc.net/1043.cpp
+++ acmicpc.net/1043.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> known;
-vector<vector<int>> party, graph;
+vector<int> state;
+vector<vector<int>> party;
 int n, m;
 int main()
 {
 	cin >> n >> m;
-	party.assign(m, vector<int>());
-	graph.assign(n + 1, vector<int>(n + 1, 0));
+	party.assign(m, vector<int>(51, 0));
+	state.assign(51, 0);
 	int t, x;
 	cin >> t;
 
 	for (int i = 0; i < t; i++)
 	{
 		cin >> x;
-		known.push_back(x);
+		state[x] = 1;
 	}
 	for (int i = 0; i < m; i++)
 	{
@@ -23,9 +23,52 @@ int main()
 		for (int j = 0; j < t; j++)
 		{
 			cin >> x;
-			party[i].push_back(x);
+			party[i][x] = 1;
 		}
 	}
 
+	for(int k = 0; k < m; k++)
+	{
+		for (int i = 0; i < m; i++)
+		{
+			bool f = false;
+			for (int j = 1; j <= n; j++)
+			{
+				if(party[i][j] && state[j])
+				{
+					f = true;
+					break;
+				}
+			}
+			if(f)
+			{
+				for(int j = 1; j <= n; j++)
+				{
+					if(party[i][j])
+					{
+						state[j] = 1;
+					}
+				}
+			}
+		}
+	}
+	int cnt = 0;
+	for (int i = 0; i < m; i++)
+	{
+		bool f = true;
+		for (int j = 1; j <= n; j++)
+		{
+			if(party[i][j] && state[j])
+			{
+				f = false;
+				break;
+			}
+		}
+		if(f)
+		{
+			cnt++;
+		}
+	}
 
+	cout << cnt << "\n";
 }
