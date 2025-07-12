7ef45ef906cf485aa22900790f70ac09cfcacf0f
Hoppouu <drone1575@gmail.com>
2025-07-12 23:09:27 +0900

    2025-07-12

---
+++ acmicpc.net/14645.cpp
@@ -0,0 +1,20 @@
+//23:09
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int a, b;
+		cin >> a >> b;
+	}
+
+	cout << "비와이\n";
+}
