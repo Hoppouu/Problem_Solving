ab53ca7b2abf8b0e3fc5e32585fcac3c1a5c22a0
Hoppouu <drone1575@gmail.com>
2025-07-13 15:19:47 +0900

    2025-07-13

---
+++ acmicpc.net/26082.cpp
@@ -0,0 +1,14 @@
+//15:19
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b, c;
+	cin >> a >> b >> c;
+
+	cout << b / a * c * 3 << "\n";
+}
