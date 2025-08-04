e751b9a2df8e331eeb15905a0990e0699bfbb82e
Lab <drone1575@naver.com>
2025-08-04 09:47:32 +0900

    2025-08-04

---
+++ acmicpc.net/34052.cpp
@@ -0,0 +1,16 @@
+//09:47
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 4; i++)
+	{
+		int n;
+		cin >> n;
+		sum += n;
+	}
+
+	cout << (sum <= 1500 ? "Yes" : "No");
+}
