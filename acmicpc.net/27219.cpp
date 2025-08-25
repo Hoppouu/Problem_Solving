d734b3b67e26e843a5a9173e9c23e61a1a22c5ba
Hoppouu <drone1575@gmail.com>
2025-08-25 21:47:22 +0900

    2025-08-25

---
+++ acmicpc.net/27219.cpp
@@ -0,0 +1,18 @@
+//21:46
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cin >> n;
+
+	for (int i = 0; i < n / 5; i++)
+	{
+		cout << "V";
+	}
+
+	for (int i = 0; i < n % 5; i++)
+	{
+		cout << "I";
+	}
+}
